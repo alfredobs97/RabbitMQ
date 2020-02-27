e980acdb15a112dca57a0420095b67e7166ea102
alfredobs97 <alfredobautista1@gmail.com>
2020-02-27 13:39:11 +0100

    add keys to email, callback to promise and config

--- src/generateEmail.js
+++ src/generateEmail.js
@@ -1,29 +1,40 @@
-const rabbit = require('./app');
+const rabbit = require('./rabbit');
 const queueEmail = 'email';
 const nodemailer = require('nodemailer');
+const config = require('./config');
 
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
+const transporter = nodemailer.createTransport({
+  service: config.SERVICE,
   auth: {
-    user: 'nuestroEmail',
-    pass: 'nuestraPass'
+    user: config.EMAIL,
+    pass: config.PASS
   }
 });
 
-function generateEmail(mail) {
-  let mailOptions = {
-    from: 'nuestroEmail',
-    to: mail,
+function generateEmail(userPreferences) {
+  const mailOptions = {
+    from: config.EMAIL,
+    to: userPreferences.email,
     subject: 'Your keys are ready!!',
-    text: 'All your keys are already encoded and ready for you to download them'
+    text:
+      'All your keys are already encoded and ready for you to download them',
+    attachments: [
+      {
+        filename: 'publicKey.pem',
+        content: Buffer.from(userPreferences.public, 'utf-8')
+      },
+      {
+        filename: 'privateKey.pem',
+        content: Buffer.from(userPreferences.private, 'utf-8')
+      }
+    ]
   };
 
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) return reject(error);
+      return resolve(info.response);
+    });
   });
 }
 
@@ -31,17 +42,15 @@ rabbit
   .start()
   .then(channel => {
     console.log('Connected');
+    
     channel.consume(queueEmail, async msg => {
-      let configurationKey = JSON.parse(msg.content);
-      console.log(' [x] Received %s', msg.content.toString());
-
-      const mail = await generateEmail(configurationKey.mail);
-      rabbit.sendKeyBack(channel, mail);
+      const keyAndEmail = JSON.parse(msg.content);
+      const responseMailServer = await generateEmail(keyAndEmail);
 
       //confirma que se ha enviado el correo y lo elimina de la cola
 
       channel.ack(msg);
-      console.log(mail);
+      console.log('Respuesta del servidor de correo: ' + responseMailServer);
     });
   })
   .catch(err => console.log(err));
