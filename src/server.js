d96a571e62b8c8d1888334d049809c64ff83422d
carlo2366 <carlobg2366@gmail.com>
2020-02-29 17:06:55 +0100

    feat(server.js)
    configure cors

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const app = express();
 const rabbit = require('./rabbit');
+const cors = require('cors');
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-
-  next();
-});
+app.use(cors());
 
 const getStartRabbit = async () => {
   rabbit
